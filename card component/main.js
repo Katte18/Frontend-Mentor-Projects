a6486523ee7d14f06767afe8022835af525a0217
Katte18 <buleett@gmail.com>
2023-02-21 20:33:30 +0200

    Add files via upload

--- card component/main.js
+++ card component/main.js
@@ -1,18 +1,59 @@
 const buttonCart = document.querySelector(".button-container");
+const shoppingCart = document.querySelector(".shopping-cart-container");
+
 const buttonClose = document.querySelector(".close-button");
-const messageCart = document.querySelector(".message");
+const buttonContinueShopping = document.querySelector(".shopping-button");
+
 const mainContent = document.querySelectorAll(".section-card-first, .section-card-second");
 
-buttonCart.addEventListener("click", () => {
-    messageCart.style.visibility = "visible";
-    mainContent.forEach(el => {
-        el.style.opacity = "0.7";
-    })
-})
+const currentPrice = document.querySelector(".current-price");
 
+const inputQuantity = document.querySelector("#item-price");
+
+const cartShoppingPrice = document.querySelector(".price");
+
+
+buttonCart.addEventListener("click", () => {
+    shoppingCart.style.visibility = "visible";
+
+    mainContent.forEach(el => {
+        el.style.opacity = "0.6";
+    });
+    inputQuantity.value = "";
+    cartShoppingPrice.innerText = "";
+});
+
+// close button
 buttonClose.addEventListener("click", () => {
-    messageCart.style.visibility = "hidden";
+    shoppingCart.style.visibility = "hidden";
     mainContent.forEach(el => {
         el.style.opacity = "1";
+    });
+    inputQuantity.value = "";
+});
+
+// continue shopping
+buttonContinueShopping.addEventListener("click",() => {
+    shoppingCart.style.visibility = "hidden";
+     mainContent.forEach(el => {
+        el.style.opacity = "1";
     })
-})
+});
+
+// price calculation
+inputQuantity.addEventListener("input", () => {
+    let valueInput = inputQuantity.value;
+    valueInput = parseFloat(valueInput);
+
+    let itemPrice = currentPrice.innerText;
+        itemPrice = parseFloat(itemPrice);
+    
+    if (typeof valueInput === "number" && valueInput >= 1) {
+        let resultTotal = (valueInput * itemPrice).toFixed(2);
+        cartShoppingPrice.innerHTML = resultTotal;
+    } if (!valueInput || valueInput === null) {
+        cartShoppingPrice.innerHTML = "";
+    };  
+});
+
+
