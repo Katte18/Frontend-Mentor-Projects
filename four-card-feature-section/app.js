ca768ab49625b6f1f3ac80846645bb53634d7269
Katte18 <buleett@gmail.com>
2024-12-13 18:18:30 +0200

    Add files via upload

---
+++ four-card-feature-section/app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+async function getDataCards() {
+  const res = await fetch("./data.json");
+  const dataCard = await res.json();
+
+  return dataCard;
+}
+
+contentCard();
+async function contentCard() {
+  const articleCard = document.querySelector("article.card");
+  const getDataCard = await getDataCards();
+  try {
+    for (const card of getDataCard.cards) {
+      articleCard.innerHTML += `
+          <div>
+            <h2>${card.title}</h2>
+            <p>${card.text}</p>
+            <figure>
+              <img src="${card.icon}" alt="${card.title} card">
+            </figure>
+          </div>`;
+    }
+  } catch ({ name, message }) {
+    articleCard.innerHTML += `
+        <h2>Error: ${name}</h2>
+        <p>Cannot retrieve json file ${message}</p>
+        <figure>
+          <img src="${name}" alt="${name} card">
+        </figure>`;
+  }
+}
